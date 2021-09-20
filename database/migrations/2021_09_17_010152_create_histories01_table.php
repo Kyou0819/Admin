c988fc614f968378da1d39f4084a7089d6449ae3
Kyou0819 <jiayi.kuang@rakuten-sec.co.jp>
2021-09-21 08:33:41 +0900

    3-13

--- database/migrations/2021_09_17_010152_create_histories01_table.php
+++ database/migrations/2021_09_17_010152_create_histories01_table.php
@@ -13,7 +13,7 @@ class CreateHistories01Table extends Migration
      */
     public function up()
     {
-        Schema::create('histories01', function (Blueprint $table) {
+        Schema::create('history01s', function (Blueprint $table) {
             $table->id();
             $table->integer('profile_id');
             $table->string('edited_at');
@@ -28,6 +28,6 @@ class CreateHistories01Table extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('histories01');
+        Schema::dropIfExists('history01s');
     }
 }
